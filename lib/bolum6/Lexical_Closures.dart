1aee8400c4627d504f7062e1bf7361f6c8a0f48b
Mustafa <18701031@mersin.edu.tr>
2020-02-24 23:44:36 +0300

    The Concept of Lexical Closures

---
+++ lib/bolum6/Lexical_Closures.dart
@@ -0,0 +1,29 @@
+/*
+* Closure özel bir fonksiyondur, closure ile bir üst kapsamdaki değişkenlerin değerlerini değiştirebiliriz.
+*/
+
+main(List<String> args) {
+  String isim = "Mustsafa";
+
+  Function ismimiGoster = () {
+    isim = "Mustafa Uysal";
+    print(isim);
+  };
+
+  ismimiGoster();
+
+  Function konus = () {
+    String selam = "Merhaba";
+
+    Function soyle = () {
+      selam = "Selamün Aleyküm";
+      print(selam);
+    };
+
+    return soyle;
+  };
+
+  var fonksiyonuAl = konus();
+
+  fonksiyonuAl();
+}
