b3ba7e9c265f8a83da46b80b3e90633e0123c975
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-05-06 23:52:44 +0300

    Update

--- lib/bolum5/Static_Method_and_Variables.dart
+++ lib/bolum5/Static_Method_and_Variables.dart
@@ -1,11 +1,3 @@
-/*
-* Sınıflarımızda nesne üretmeden bazı değişkenlere ve methodlara erişmek isteyebiliriz. Burada karşımıza static kavramı çıkar.
-* Static kullanarak nesne değil sınıf değişkenleri ve methodları oluşturabiliriz.
-* Bir sınıfta tanımlanan method ve değişkenlere erişirken mutlaka nesne üretmemiz gerekirdi. Ama biz bunları static olarak tanımlarsak
-* artık nesne üretmeden bu değerlere erişebilir ve kullanabiliriz. Artık onlar nesnelerin değil sınıfın method ve değişkenleridir.
-*/
-
-
 main(List<String> args) {
 
   var mustafa = Ogrenci();
