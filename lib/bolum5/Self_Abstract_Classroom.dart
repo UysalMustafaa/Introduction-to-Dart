ce22a4a3337a45bcc42ca183062d61f8b9a1755b
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-06-07 23:13:41 +0300

    Self Class and Methods

--- lib/bolum5/Self_Abstract_Classroom.dart
+++ lib/bolum5/Self_Abstract_Classroom.dart
@@ -1,10 +1,3 @@
-/* 
-* Abstract Soyut Sınıf : Sınıflar arasında soyutlama yapmak için kullanılır.
-* Abstract sınıflardan nesne üretilemez, abstract sınıflarda normal ve abtract methodlar olur.
-* Abstract methodlar alt sınıflarca override edilmek zorundadır.
-* Bir sınıfta bir tane abstract method varsa o sınıf mutlaka abstract tanımlanmalıdır.
-*/
-
 main(List<String> args) {
   
   var k1 = Kare();
