49e59485066f61d2d2811d8cd81b0c2ed8a228b8
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-06-09 23:10:41 +0300

    Operators

--- lib/bolum2/Operators.dart
+++ lib/bolum2/Operators.dart
@@ -1,5 +1,31 @@
 main(List<String> args) {
   
+  double sayi1 = 9;
+  double sayi2 = 6;
+
+
+
+  print("$sayi1 + $sayi2 Toplamı ${sayi1 + sayi2}");
+  print("$sayi1 - $sayi2 Farkı ${sayi1 - sayi2}");
+  print("$sayi1 * $sayi2 Çarpımı ${sayi1 * sayi2}");
+  print("$sayi1 / $sayi2 Bölümü ${sayi1 / sayi2}");
+  print("$sayi1 % $sayi2 Mod ${sayi1 % sayi2}");
+
+
+
+double sayi3 = 5;
+sayi3 = sayi3 + 5;
+print(sayi3);
+
+sayi3 += 5; // sayi3 = sayi3 + 5
+print(sayi3);
+
+sayi3 %= 4;
+print(sayi3);
+
+double sayi4 = 9;
+double sayi5 = 5;
+
  
 if (sayi4 <= sayi5) {
 
